e771bffdd17f8a925856bac9d369ad65326cf734
Aji Sulistyo Nugroho <ajisulist@gmail.com>
2024-01-23 10:01:57 +0000

    fix runner

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-import { readdir, watch } from "fs/promises";
+import { readdir } from "fs/promises";
+import { watchFile } from "fs";
 import select from "@inquirer/select";
-import { spawn } from "child_process";
+import { exec, spawn } from "child_process";
 import path from "path";
 import chalk from "chalk";
 const __BASE_DIRECTORY = "./src";
 
 const clearConsole = () => {
-  console.clear();
+  process.stdout.write("\x1Bc");
 };
 
 const listDirectories = async () => {
@@ -16,15 +17,16 @@ const listDirectories = async () => {
 
 const executeFile = (fullPath) => {
   const childProcess = spawn("node", [`${fullPath}`]);
-
+  let a = 0;
   childProcess.stdout.on("data", (data) => {
-    clearConsole();
-    console.log(chalk.green(`${data}`));
+    process.stdout.write(chalk.green(`${data}`));
   });
 
   childProcess.stderr.on("data", (data) => {
+    process.stdout.write(chalk.red(`${data}`));
+  });
+  childProcess.on("spawn", () => {
     clearConsole();
-    console.log(chalk.red(`${data}`));
   });
 };
 
@@ -32,10 +34,9 @@ const watchAndExecute = async (directory) => {
   const fullFilename = `${__BASE_DIRECTORY}/${directory}/index.js`;
   const fullPath = path.resolve(fullFilename);
   executeFile(fullPath);
-  const watcher = watch(fullFilename);
-  for await (const event of watcher) {
+  watchFile(fullFilename, () => {
     executeFile(fullPath);
-  }
+  });
 };
 
 const showMenu = async () => {
