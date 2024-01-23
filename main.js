c98e4c07768fed3266d811e8c105b4f463be6c93
Aji Sulistyo Nugroho <ajisulist@gmail.com>
2024-01-23 12:13:59 +0000

    simplify runner

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 import { readdir } from "fs/promises";
-import { watchFile } from "fs";
 import select from "@inquirer/select";
-import { exec, spawn } from "child_process";
+import { fork } from "child_process";
 import path from "path";
-import chalk from "chalk";
 const __BASE_DIRECTORY = "./src";
 
 const clearConsole = () => {
@@ -15,27 +13,16 @@ const listDirectories = async () => {
   return list;
 };
 
-const executeFile = (fullPath) => {
-  const childProcess = spawn("node", [`${fullPath}`]);
-  let a = 0;
-  childProcess.stdout.on("data", (data) => {
-    process.stdout.write(chalk.green(`${data}`));
-  });
-
-  childProcess.stderr.on("data", (data) => {
-    process.stdout.write(chalk.red(`${data}`));
-  });
-  childProcess.on("spawn", () => {
-    clearConsole();
-  });
-};
-
-const watchAndExecute = async (directory) => {
+const executeFile = async (directory) => {
   const fullFilename = `${__BASE_DIRECTORY}/${directory}/index.js`;
   const fullPath = path.resolve(fullFilename);
-  executeFile(fullPath);
-  watchFile(fullFilename, () => {
-    executeFile(fullPath);
+  const childProcess = fork(fullPath, {
+    stdio: "inherit",
+    execArgv: ["--watch"],
+  });
+
+  childProcess.on("spawn", () => {
+    clearConsole();
   });
 };
 
@@ -50,7 +37,7 @@ const showMenu = async () => {
     })),
   });
   clearConsole();
-  await watchAndExecute(answer);
+  await executeFile(answer);
 };
 
 showMenu();
