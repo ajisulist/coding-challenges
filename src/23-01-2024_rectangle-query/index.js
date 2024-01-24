691b2385a968556437234952429a842eb5fb250a
Aji Sulistyo Nugroho <ajisulist@gmail.com>
2024-01-24 17:05:02 +0700

    rectangle query

--- src/23-01-2024_rectangle-query/index.js
+++ src/23-01-2024_rectangle-query/index.js
@@ -1,1 +1,91 @@
-console.log("Aji\nTesttt");
+function createRectangle(h, w) {
+  // console.log(h, w);
+  return Array(h).fill(Array(w).fill(1));
+}
+
+function executeQuery({
+  rectangle,
+  rowNumber,
+  colNumber,
+  rowIndex,
+  colIndex,
+  operator,
+}) {
+  if (rowIndex < rowNumber && colIndex < colNumber) {
+    switch (operator) {
+      case ">":
+        for (let i = colIndex + 1; i < colNumber; i++) {
+          if (rectangle[rowIndex][i] === 1) {
+            return [rowIndex, i];
+          }
+        }
+        break;
+      case "<":
+        for (let i = colIndex - 1; i >= 0; i--) {
+          if (rectangle[rowIndex][i] === 1) {
+            return [rowIndex, i];
+          }
+        }
+        break;
+      case "^":
+        for (let i = rowIndex - 1; i >= 0; i--) {
+          if (rectangle[i][colIndex] === 1) {
+            return [i, colIndex];
+          }
+        }
+        break;
+      case "v":
+        for (let i = rowIndex + 1; i < rowNumber; i++) {
+          if (rectangle[i][colIndex] === 1) {
+            return [i, colIndex];
+          }
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  return [-1, -1];
+}
+
+function solution({ height, width, queries }) {
+  let rectangle = createRectangle(height, width);
+  let queryResult = [];
+
+  for (const query of queries) {
+    const [queryOperator, queryRow, queryCol] = query.split(" ");
+    const rowIndex = Number(queryRow);
+    const colIndex = Number(queryCol);
+
+    if (queryOperator === "x") {
+      rectangle = [
+        ...rectangle.slice(0, rowIndex),
+        [
+          ...rectangle[rowIndex].slice(0, colIndex),
+          0,
+          ...rectangle[rowIndex].slice(colIndex + 1),
+        ],
+        ...rectangle.slice(rowIndex + 1),
+      ];
+    } else {
+      queryResult = [
+        ...queryResult,
+        executeQuery({
+          rectangle,
+          rowNumber: height,
+          colNumber: width,
+          rowIndex,
+          colIndex,
+          operator: queryOperator,
+        }),
+      ];
+    }
+  }
+  console.log(queryResult);
+}
+
+solution({
+  height: 3,
+  width: 5,
+  queries: ["v 1 2", "x 2 2", "v 1 2", "> 2 1", "x 2 3", "> 2 1", "< 2 0"],
+});
---
+++ src/23-01-2024_rectangle-query/index.md
@@ -0,0 +1,16 @@
+Consider a rectangular h × w board with all cells initially white. You are to process several queries of the following types:
+
+"x a b" - color the white cell (a, b) (0-based coordinates, the first one is a row index, and the second one is a column index) black;
+"> a b" - find the leftmost white cell to the right of the white cell (a, b);
+"< a b" - find the rightmost white cell to the left of the white cell (a, b);
+"v a b" - the same, but the search should be done downwards;
+"^ a b" - the same, but the search should be done upwards;
+For each query, except the ones of the first type, find the answer.
+
+Example
+
+For h = 3, w = 5, and
+queries = ["v 1 2", "x 2 2", "v 1 2", "> 2 1", "x 2 3", "> 2 1", "< 2 0"],
+the output should be
+solution(h, w, queries) = [[2, 2], [-1, -1], [2, 3], [2, 4], [-1, -1]].
+
